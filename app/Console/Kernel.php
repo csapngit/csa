90d45642ddc2bf89dab65a1e0d616be81f1c654c
vagrant <vagrant@homestead>
2023-08-04 08:09:43 +0000

    Matiin scheduler order

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -41,72 +41,72 @@ class Kernel extends ConsoleKernel
 			->at('05:00');
 
 
-		$schedule->call(new OrderDetail)
-			->at('08:25')
-			->days([1, 2, 3, 4, 5, 6]);
-		$schedule->call(new OrderDetail)
-			->at('08:55')
-			->days([1, 2, 3, 4, 5, 6]);
-		$schedule->call(new OrderDetail)
-			->at('09:25')
-			->days([1, 2, 3, 4, 5, 6]);
-		$schedule->call(new OrderDetail)
-			->at('09:55')
-			->days([1, 2, 3, 4, 5, 6]);
-		$schedule->call(new OrderDetail)
-			->at('10:25')
-			->days([1, 2, 3, 4, 5, 6]);
-		$schedule->call(new OrderDetail)
-			->at('10:55')
-			->days([1, 2, 3, 4, 5, 6]);
-		$schedule->call(new OrderDetail)
-			->at('11:25')
-			->days([1, 2, 3, 4, 5, 6]);
-		$schedule->call(new OrderDetail)
-			->at('11:55')
-			->days([1, 2, 3, 4, 5, 6]);
-		$schedule->call(new OrderDetail)
-			->at('12:25')
-			->days([1, 2, 3, 4, 5, 6]);
-		$schedule->call(new OrderDetail)
-			->at('12:55')
-			->days([1, 2, 3, 4, 5, 6]);
-		$schedule->call(new OrderDetail)
-			->at('13:25')
-			->days([1, 2, 3, 4, 5, 6]);
-		$schedule->call(new OrderDetail)
-			->at('13:55')
-			->days([1, 2, 3, 4, 5, 6]);
-		$schedule->call(new OrderDetail)
-			->at('14:25')
-			->days([1, 2, 3, 4, 5, 6]);
-		$schedule->call(new OrderDetail)
-			->at('14:55')
-			->days([1, 2, 3, 4, 5, 6]);
-		$schedule->call(new OrderDetail)
-			->at('15:25')
-			->days([1, 2, 3, 4, 5, 6]);
-		$schedule->call(new OrderDetail)
-			->at('15:55')
-			->days([1, 2, 3, 4, 5, 6]);
-		$schedule->call(new OrderDetail)
-			->at('16:25')
-			->days([1, 2, 3, 4, 5, 6]);
-		$schedule->call(new OrderDetail)
-			->at('16:55')
-			->days([1, 2, 3, 4, 5, 6]);
-		$schedule->call(new OrderDetail)
-			->at('17:25')
-			->days([1, 2, 3, 4, 5, 6]);
-		$schedule->call(new OrderDetail)
-			->at('17:55')
-			->days([1, 2, 3, 4, 5, 6]);
-		$schedule->call(new OrderDetail)
-			->at('18:25')
-			->days([1, 2, 3, 4, 5, 6]);
-		$schedule->call(new OrderDetail)
-			->at('18:55')
-			->days([1, 2, 3, 4, 5, 6]);
+		// $schedule->call(new OrderDetail)
+		// 	->at('08:25')
+		// 	->days([1, 2, 3, 4, 5, 6]);
+		// $schedule->call(new OrderDetail)
+		// 	->at('08:55')
+		// 	->days([1, 2, 3, 4, 5, 6]);
+		// $schedule->call(new OrderDetail)
+		// 	->at('09:25')
+		// 	->days([1, 2, 3, 4, 5, 6]);
+		// $schedule->call(new OrderDetail)
+		// 	->at('09:55')
+		// 	->days([1, 2, 3, 4, 5, 6]);
+		// $schedule->call(new OrderDetail)
+		// 	->at('10:25')
+		// 	->days([1, 2, 3, 4, 5, 6]);
+		// $schedule->call(new OrderDetail)
+		// 	->at('10:55')
+		// 	->days([1, 2, 3, 4, 5, 6]);
+		// $schedule->call(new OrderDetail)
+		// 	->at('11:25')
+		// 	->days([1, 2, 3, 4, 5, 6]);
+		// $schedule->call(new OrderDetail)
+		// 	->at('11:55')
+		// 	->days([1, 2, 3, 4, 5, 6]);
+		// $schedule->call(new OrderDetail)
+		// 	->at('12:25')
+		// 	->days([1, 2, 3, 4, 5, 6]);
+		// $schedule->call(new OrderDetail)
+		// 	->at('12:55')
+		// 	->days([1, 2, 3, 4, 5, 6]);
+		// $schedule->call(new OrderDetail)
+		// 	->at('13:25')
+		// 	->days([1, 2, 3, 4, 5, 6]);
+		// $schedule->call(new OrderDetail)
+		// 	->at('13:55')
+		// 	->days([1, 2, 3, 4, 5, 6]);
+		// $schedule->call(new OrderDetail)
+		// 	->at('14:25')
+		// 	->days([1, 2, 3, 4, 5, 6]);
+		// $schedule->call(new OrderDetail)
+		// 	->at('14:55')
+		// 	->days([1, 2, 3, 4, 5, 6]);
+		// $schedule->call(new OrderDetail)
+		// 	->at('15:25')
+		// 	->days([1, 2, 3, 4, 5, 6]);
+		// $schedule->call(new OrderDetail)
+		// 	->at('15:55')
+		// 	->days([1, 2, 3, 4, 5, 6]);
+		// $schedule->call(new OrderDetail)
+		// 	->at('16:25')
+		// 	->days([1, 2, 3, 4, 5, 6]);
+		// $schedule->call(new OrderDetail)
+		// 	->at('16:55')
+		// 	->days([1, 2, 3, 4, 5, 6]);
+		// $schedule->call(new OrderDetail)
+		// 	->at('17:25')
+		// 	->days([1, 2, 3, 4, 5, 6]);
+		// $schedule->call(new OrderDetail)
+		// 	->at('17:55')
+		// 	->days([1, 2, 3, 4, 5, 6]);
+		// $schedule->call(new OrderDetail)
+		// 	->at('18:25')
+		// 	->days([1, 2, 3, 4, 5, 6]);
+		// $schedule->call(new OrderDetail)
+		// 	->at('18:55')
+		// 	->days([1, 2, 3, 4, 5, 6]);
 
 
 		// ->onSuccess(function () {
