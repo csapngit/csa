7d23022a4a10a0cd7a651efc7fd36c61bc042020
unknown <csapngit@csahome.com>
2023-11-20 12:05:05 +0700

    Fixing

--- app/Http/Controllers/Logistic/DelmanController.php
+++ app/Http/Controllers/Logistic/DelmanController.php
@@ -52,7 +52,7 @@ class DelmanController extends Controller
 		$reason = MasterStatus::where('module', 'delman')->get();
 
 		$tmp = DB::connection('192.168.11.24')->table('tds_orderdata')->where('OrderNo', $route->nomor_so)->first();
-		return $tmp;
+		// return $tmp;
 		$linkFoto = '';
 		if ($tmp) {
 			$linkFoto = $tmp->LinkFoto2;
